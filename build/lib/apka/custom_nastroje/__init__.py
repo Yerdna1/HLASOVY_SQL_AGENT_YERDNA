be58da1b27c0b98e96bd3cac4c7e5464ab96cd59
andrej <andrej.galad@gmail.com>
2025-04-06 07:39:54 +0200

    pridane_tooly

--- build/lib/apka/custom_nastroje/__init__.py
+++ build/lib/apka/custom_nastroje/__init__.py
@@ -3,18 +3,25 @@
 # Import preložených nástrojov
 from .graf import nakresli_plotly_graf
 from .databaza import vykonaj_sql
+from .email import draft_email
+from .browser import open_browser
+from .python_file import  execute_python_file, create_python_file
+from .linkedin import draft_linkedin_post
+from .image import generate_image
+from .stock import query_stock_price
+from .search import internet_search
 
 # Zoznam všetkých nástrojov
 nastroje = [
     # TODO: Pridať späť importy a nástroje, ak budú implementované
-    # query_stock_price,
-    # generate_image,
-    # internet_search,
-    # draft_linkedin_post,
-    # create_python_file,
-    # execute_python_file,
-    # open_browser,
-    # draft_email,
+     query_stock_price,
+     generate_image,
+     internet_search,
+     draft_linkedin_post,
+     create_python_file,
+     execute_python_file,
+     open_browser,
+     draft_email,
     nakresli_plotly_graf,
     vykonaj_sql,
 ]
